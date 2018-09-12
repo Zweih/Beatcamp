16e7644ef49cc28b4ebce6d693dda529125efba8
Zweih <fernandon299@gmail.com>
2018-09-12 15:13:27 -0400

    User model validation fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   attr_reader :password
   
-  validates :username, :session_token, :password_digest, presence: :true
+  validates :username, :session_token, :password_digest, presence: true
   validates :username, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
   
