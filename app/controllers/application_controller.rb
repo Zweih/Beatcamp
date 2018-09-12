66159058b961be9f9713e473c6d1744fed36b97b
Zweih <fernandon299@gmail.com>
2018-09-12 15:43:25 -0400

    Add session helper methods to application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def log_in!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def redirect_unless_logged_in
+    render json: ["Not logged in"], status: 404
+  end
 end
