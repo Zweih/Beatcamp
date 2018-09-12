ea988e858087dfc8b42e675f3746295f2b01905f
Zweih <fernandon299@gmail.com>
2018-09-12 16:03:39 -0400

    Backend routing fix.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # namespace :api, defaults: {format: :json} do
-  #   resources :benches, only: [:index, :show, :create]
-  #   resources :reviews, only: [:create]
-  #   resource :user, only: [:create]
-  #   resource :session, only: [:create, :destroy, :show]
-  #   resource :favorites, only: [:create, :destroy]
-  # end
-
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
