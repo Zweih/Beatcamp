36d0d6e61b151b9a85987d94f3b212e9442dba96
Zweih <fernandon299@gmail.com>
2018-09-12 14:42:07 -0400

    Finish user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,42 @@
 class User < ApplicationRecord
   attr_reader :password
-
+  
   validates :username, :session_token, :password_digest, presence: :true
   validates :username, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
   
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+  
   def password=(password)
-    # TODO:
+    @password = password
+    self.password = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
   end
 end
