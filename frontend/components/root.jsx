cb65e744f08b984ba6d71610ec8bef2c0210199d
Zweih <fernandon299@gmail.com>
2018-09-12 17:08:58 -0400

    Root JSX formatting fix.

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
-
 import App from "./app";
 
-// NB: this file is complete - you do not to write/edit anything!
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
