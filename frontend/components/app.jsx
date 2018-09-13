aa234238b3c722db6b7b225126264edbf3b06ed5
Zweih <fernandon299@gmail.com>
2018-09-13 13:47:00 -0400

    Formatting fix.

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -30,5 +30,4 @@ const App = () => (
   </div>
 );
 
-
 export default App;
---
+++ roundabout.sh
@@ -0,0 +1,7 @@
+git checkout development
+git push origin development
+git checkout master
+git merge master
+git push origin master
+git push heroku
+git checkout development
