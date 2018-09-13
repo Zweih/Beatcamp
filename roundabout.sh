ddb6a9c2a73294ae3b1c4237219219c813fc2076
Zweih <fernandon299@gmail.com>
2018-09-13 13:49:16 -0400

    Add bash script for merge pushing.

--- roundabout.sh
+++ roundabout.sh
@@ -1,7 +1,7 @@
 git checkout development
 git push origin development
 git checkout master
-git merge master
+git merge development
 git push origin master
 git push heroku
 git checkout development
