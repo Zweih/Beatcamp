4061deb42e331155c5f657197e4cd68740a58f03
Zweih <fernandon299@gmail.com>
2018-09-14 15:11:04 -0400

    Finish styling header. Refactor header out of App component.

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -13,42 +13,11 @@ import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import GreetingContainer from "./greeting/greeting_container";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
+import Main from "./main";
 
 const App = () => (
   <div>
-    <header>
-      <div className="left-header">
-        <Link to="/" className="logo">
-          <img src="https://my.mixtape.moe/eohwhz.png"/>
-        </Link>
-        <p>
-          Discover amazing new music and directly support the artists who make it.
-        </p>
-      </div>
-      <div className="right-header">
-        <form className="search-form">
-          <input
-            className="search-bar"
-            type="text"
-            placeholder="Search for artist, track or album"
-          />
-          <span className="search-submit-span">
-            <button 
-              className="search-submit"
-              type="submit"
-            >
-              <i className="fas fa-search fa-lg"></i>
-            </button>
-          </span>
-        </form>
-        <GreetingContainer />
-      </div>
-    </header>
-
-    <Switch>
-      <AuthRoute exact path="/login" component={LoginFormContainer}/>
-      <AuthRoute exact path="/signup" component={SignupFormContainer}/>
-    </Switch>
+    <Main />
   </div>
 );
 
