2743b849f5fbce3fe8d3e25fe564807427739948
Zweih <fernandon299@gmail.com>
2018-10-18 14:34:08 -0400

    Add Tracks reducer to entities reducer.

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import users from "./users_reducer";
-import albums from "./albums_reducer"
+import albums from "./albums_reducer";
+import tracks from "./tracks_reducer";
 
 const entitiesReducer = combineReducers ({
   users,
-  albums,
+	albums,
+	tracks,
 });
 
 export default entitiesReducer;
