a0d78bd3b1adbc5d3ac21104310ba40f72010647
Zweih <fernandon299@gmail.com>
2018-10-18 14:35:38 -0400

    Add track errors reducer to errors reducer.

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,11 +1,12 @@
 import session from "./session_errors_reducer";
 import album from "./album_errors_reducer";
+import track from "./track_errors_reducer";
 import { combineReducers } from "redux";
 
 const errorsReducer = combineReducers({
   session,
-  album,
-  // TODO: add errors for user
+	album,
+	track,
 });
 
 export default errorsReducer;
