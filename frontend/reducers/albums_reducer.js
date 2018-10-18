09459b6a9f3f20c52b50de2061f83e2c270b313d
Zweih <fernandon299@gmail.com>
2018-10-18 14:33:19 -0400

    Add Tracks reducer. Fix indentation.

--- frontend/reducers/albums_reducer.js
+++ frontend/reducers/albums_reducer.js
@@ -1,34 +1,35 @@
-import { 
-  RECEIVE_CURRENT_USER,
-  RECEIVE_USER
- } from "../actions/session_actions";
+import {
+	RECEIVE_CURRENT_USER,
+	RECEIVE_USER
+} from "../actions/session_actions";
 
- import {
-   RECEIVE_ALBUM,
-   RECEIVE_ALL_ALBUMS
- } from "../actions/album_actions";
+import {
+	 RECEIVE_ALBUM,
+	 RECEIVE_ALL_ALBUMS
+} from "../actions/album_actions";
+
 import merge from "lodash/merge";
 
 const albumsReducer = (state = {}, action) => {
-  Object.freeze(state);
+	Object.freeze(state);
 
-  switch (action.type) {
-    case RECEIVE_CURRENT_USER:
-    case RECEIVE_USER:
-      const albums = {};
+	switch (action.type) {
+		case RECEIVE_CURRENT_USER:
+		case RECEIVE_USER:
+			const albums = {};
 
-      Object.values(action.albums).forEach( (album) => { 
-        return albums[album.id] = album;
-      });
+			Object.values(action.albums).forEach( (album) => {
+				return albums[album.id] = album;
+			});
 
-      return merge({}, state.albums, albums);
-    case RECEIVE_ALBUM:
-      return merge({}, state, { [action.album.id]: action.album });
-    case RECEIVE_ALL_ALBUMS:
-      return merge({}, state, action.albums);
-    default:
-      return state;
-  }
+			return merge({}, state.albums, albums);
+		case RECEIVE_ALBUM:
+			return merge({}, state, { [action.album.id]: action.album });
+		case RECEIVE_ALL_ALBUMS:
+			return merge({}, state, action.albums);
+		default:
+			return state;
+	}
 };
 
 export default albumsReducer;
---
+++ frontend/reducers/tracks_reducer.js
@@ -0,0 +1,33 @@
+import {
+	 RECEIVE_ALBUM
+ } from "../actions/album_actions";
+
+import {
+		RECEIVE_TRACK,
+		RECEIVE_ALL_TRACKS
+	} from "../actions/track_actions";
+
+import merge from "lodash/merge";
+
+const tracksReducer = (state = {}, action) => {
+	Object.freeze(state);
+
+	switch (action.type) {
+		case RECEIVE_ALBUM:
+			const tracks = {};
+
+			Object.values(action.tracks).forEach( (track) => { 
+				return tracks[track.id] = track;
+			});
+
+			return merge({}, state.tracks, tracks);
+		case RECEIVE_TRACK:
+			return merge({}, state, { [action.track.id]: action.track });
+		case RECEIVE_ALL_TRACKS:
+			return merge({}, state, action.tracks);
+		default:
+			return state;
+	}
+};
+
+export default tracksReducer;
