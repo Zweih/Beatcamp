5711b4991bfd9deef0d77ed112fa1a6d533dcc54
Zweih <fernandon299@gmail.com>
2018-10-18 13:30:00 -0400

    Fix indentation. Add user avatar to homepage nav.

--- frontend/components/greeting/left_greeting.jsx
+++ frontend/components/greeting/left_greeting.jsx
@@ -2,33 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const LeftGreeting = ({ currentUser }) => {
-  const notLoggedIn = () => (
-    <p className="header-text">
-      Discover amazing new music and directly support the artists who make it.
-    </p>
-  );
+	const notLoggedIn = () => (
+		<p className="header-text">
+			Discover amazing new music and directly support the artists who make it.
+		</p>
+	);
 
-  const loggedIn = () => (
-    <ul className="header-links">
-      <li>
-        <p className="header-link">Hi {currentUser.username}</p>
-      </li>
-      <li>
-        <Link 
-          className="header-link"
-          to={`/users/${currentUser.id}`}
-        >
-          collection
-        </Link>
-      </li>
-    </ul>
-  );
+	const loggedIn = () => (
+		<ul className="header-links">
+			<li>
+				<img src={currentUser.avatar_url}></img>
+				<p className="header-link username">Hi {currentUser.username}</p>
+			</li>
+			<li>
+				<Link 
+					className="header-link"
+					to={`/users/${currentUser.id}`}
+				>
+					collection
+				</Link>
+			</li>
+		</ul>
+	);
 
-  return (
-    <nav className="left-greeting">
-      {currentUser ? loggedIn() : notLoggedIn()}
-    </nav>
-  );
+	return (
+		<nav className="left-greeting">
+			{currentUser ? loggedIn() : notLoggedIn()}
+		</nav>
+	);
 }
 
 export default LeftGreeting;
