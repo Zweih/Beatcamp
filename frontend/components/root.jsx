c4938bbc099dc84c500c9248e47fc8873e663ccc
Zweih <fernandon299@gmail.com>
2018-10-23 15:02:32 -0400

    Remove test methods.

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,11 +4,7 @@ import { HashRouter } from "react-router-dom";
 import App from "./app";
 import * as TrackApiUtil from "../util/track_api_util";
 
-const Root = ({ store }) => {
-	window.createTrack = TrackApiUtil.createTrack;
-	window.fetchTrack = TrackApiUtil.fetchTrack;
-	window.fetchTracks = TrackApiUtil.fetchTracks;
-  
+const Root = ({ store }) => {  
   return (
     <Provider store={store}>
       <HashRouter>
