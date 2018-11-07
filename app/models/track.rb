486205679fcf1f09fe1fe6c1d16a0be5c863fd50
Zweih <fernandon299@gmail.com>
2018-11-07 13:44:18 -0500

    Add method to parse duration from file on backend.

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,5 @@
+require 'taglib' if File.basename($PROGRAM_NAME) != 'rake'
+
 class Track < ApplicationRecord
 	validates :title, :album_id, :list_num, presence: true
 
@@ -12,5 +14,21 @@ class Track < ApplicationRecord
 		else
 			""
 		end
+	end
+
+	def audio_len
+		len = 0
+		audio_path = "#{Rails.root.join('tmp').to_s}/#{self.audio.filename}"
+		
+		File.open(audio_path, 'wb') do |file|
+			file.write(self.audio.download)
+		end
+
+		TagLib::MPEG::File.open(audio_path) do |file|
+			properties = file.audio_properties
+			len = properties.length
+		end
+
+		return len
 	end
 end
