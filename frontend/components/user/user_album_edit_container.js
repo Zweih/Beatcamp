ce90d3a4563a108262d9056d58bf70d70b09845f
Zweih <fernandon299@gmail.com>
2018-11-20 15:10:52 -0500

    Finish UserAlbumEditContainer.

--- frontend/components/user/user_album_edit_container.js
+++ frontend/components/user/user_album_edit_container.js
@@ -1,24 +1,37 @@
 import { connect } from "react-redux";
 import UserAlbumEdit from "./user_album_edit";
 import { 
-	// TODO: ADD ALBUM UPDATE,
-	clearSessionErrors } from "../../actions/session_actions";
+	clearAlbumErrors,
+	fetchAlbum,
+	updateAlbum } from "../../actions/album_actions";
 
-const mapStateToProps = (state) => {
+import { selectUserAlbum } from "../../reducers/selectors";
+
+const mapStateToProps = (state, ownProps) => {
 	const currentUser = state.entities.users[state.session.id];
-	const userAlbum = selectUserAlbum(state.entities, albumId); 
-	const errors = state.errors.session;
+	const albumUserId = parseInt(ownProps.match.params.userId);
+	const albumId = parseInt(ownProps.match.params.albumId);
+	const userAlbum = selectUserAlbum(state.entities, albumId);
+	const errors = state.errors.album;
 	const formType = "Album Edit";
 	const formClass = "a-edit";
 
-	return { errors, formType, formClass, currentUser, userAlbum };
+	return {
+		errors,
+		formType,
+		formClass,
+		currentUser,
+		userAlbum,
+		albumId,
+		albumUserId,
+	};
 };
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		processForm: (user) => dispatch(updateUser(user)),
+		processForm: (album, user) => dispatch(updateAlbum(album, user)),
 		fetchAlbum: (albumId) => dispatch(fetchAlbum(albumId)),
-		clearSessionErrors: () => dispatch(clearSessionErrors()),
+		clearAlbumErrors: () => dispatch(clearAlbumErrors()),
 	};
 };
 
