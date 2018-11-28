8fd1c8e5996f1cc427c471b50ba44015510d7208
Zweih <fernandon299@gmail.com>
2018-11-27 20:46:59 -0500

    Formatting fix.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, "frontend", "entry.jsx"),
-  output: {
-    path: path.join(__dirname, "app", "assets", "javascripts"),
-    filename: "bundle.js"
-  },
-  resolve: {
-    extensions: [".js", ".jsx", "*"]
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(jsx)$/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        },
-        exclude: /(node_modules)/
-      }
-    ]
+	entry: path.join(__dirname, "frontend", "entry.jsx"),
+	output: {
+		path: path.join(__dirname, "app", "assets", "javascripts"),
+		filename: "bundle.js"
+	},
+	resolve: {
+		extensions: [".js", ".jsx", "*"]
+	},
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.(jsx)$/,
+				use: {
+					loader: 'babel-loader',
+					query: {
+						presets: ['@babel/env', '@babel/react']
+					}
+				},
+				exclude: /(node_modules)/
+			}
+		]
 	},
 };
